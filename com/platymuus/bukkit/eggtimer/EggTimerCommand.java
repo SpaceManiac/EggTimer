61a3448e5cf961db5732385b178678067e1f95d0
SpaceManiac <tad.hardesty@suddenlink.net>
2011-03-21 21:31:58 -0500

    Added more stub functions

--- com/platymuus/bukkit/eggtimer/EggTimerCommand.java
+++ com/platymuus/bukkit/eggtimer/EggTimerCommand.java
@@ -1,14 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.platymuus.bukkit.eggtimer;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 /**
- *
+ * Handler for the /eggtimer command to admin EggTimer
  * @author Tad
  */
-public class EggTimerCommand {
+public class EggTimerCommand implements CommandExecutor {
 
+    private final EggTimerPlugin plugin;
+
+    public EggTimerCommand(EggTimerPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+        return false;
+    }
+    
 }
--- com/platymuus/bukkit/eggtimer/EggTimerPlugin.java
+++ com/platymuus/bukkit/eggtimer/EggTimerPlugin.java
@@ -11,13 +11,43 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class EggTimerPlugin extends JavaPlugin {
 
+    @Override
     public void onEnable() {
         // Register our commands
         getCommand("eggtimer").setExecutor(new EggTimerCommand(this));
 
+        // Start the timer - every 20 ticks = every second
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() { timer(); }
+        }, 0, 20);
+
         // Say hi
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println(pdfFile.getName() + " v" + pdfFile.getVersion() + " enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        // I think it does this anyways, but just to make sure.
+        getServer().getScheduler().cancelTasks(this);
+    }
+
+    public void timer() {
+        // TODO
+    }
+
+    public void loadConfig() {
+        this.getDataFolder().mkdirs();
+        File f = new File(this.getDataFolder().getPath() + File.pathSeparator + "locations.txt");
+        try {
+            Scanner in = new Scanner(f);
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+            }
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("[EggTimer] Warning: could not find locations.txt.");
+        }
     }
 
 }
