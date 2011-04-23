0bab7c7f15e6e4cdcbd6f4e45ee35c366d555f80
SpaceManiac <tad.hardesty@suddenlink.net>
2011-04-23 12:46:08 -0500

    Bunch of changes for v1.1.

--- com/platymuus/bukkit/eggtimer/EggTimerPlugin.java
+++ com/platymuus/bukkit/eggtimer/EggTimerPlugin.java
@@ -6,7 +6,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
+import org.bukkit.block.ContainerBlock;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -21,6 +23,9 @@ public class EggTimerPlugin extends JavaPlugin {
 
     private final ArrayList<TimerEntry> timerEntries = new ArrayList<TimerEntry>();
     private int currentTime = 0;
+
+    private boolean requireLoaded = true;
+    private boolean clearFirst = true;
 
     @Override
     public void onEnable() {
@@ -49,19 +54,29 @@ public class EggTimerPlugin extends JavaPlugin {
     private void timer() {
         ++currentTime;
 
+        List<Block> alreadyCleared = new ArrayList<Block>();
+
         for (TimerEntry entry : timerEntries) {
             if (currentTime % entry.interval == 0) {
                 World world = getServer().getWorld(entry.world);
                 Block block = world.getBlockAt(entry.x, entry.y, entry.z);
-                if (world.isChunkLoaded(world.getChunkAt(block))) {
-                    // Don't add items for unloaded chunks.
-                    if (block.getType() == Material.CHEST) {
-                        Chest chest = (Chest) block.getState();
-                        Inventory inv = chest.getInventory();
-                        // Clear it for now.
-                        inv.clear();
-                        ItemStack stack = new ItemStack(entry.item, entry.count, (short) entry.data, (byte) entry.data);
-                        inv.addItem(stack);
+
+                if (!requireLoaded || world.isChunkLoaded(world.getChunkAt(block))) {
+                    BlockState state = block.getState();
+                    if (state instanceof ContainerBlock) {
+                        Inventory inv = ((ContainerBlock) state).getInventory();
+
+                        if (clearFirst && !alreadyCleared.contains(block)) {
+                            inv.clear();
+                            alreadyCleared.add(block);
+                        }
+
+                        if (entry.item > 0) {
+                            ItemStack stack = new ItemStack(entry.item, entry.count, (short) entry.data, (byte) entry.data);
+                            inv.addItem(stack);
+                        } else if (!clearFirst) {
+                            inv.clear();
+                        }
                     }
                 }
             }
@@ -71,76 +86,90 @@ public class EggTimerPlugin extends JavaPlugin {
     public void loadConfig(Player player) {
         timerEntries.clear();
         this.getDataFolder().mkdirs();
+
+        int line = 0;
         File f = new File(getDataFolder(), "locations.txt");
+        Scanner in;
+
         try {
-            int line = 0;
-            Scanner in = new Scanner(f);
-            while (in.hasNextLine()) {
-                ++line;
-
-                String[] split = in.nextLine().split("=");
-                if (split.length < 2) continue;
-                if (split[0].charAt(0) == '#') continue;
-
-                String[] coords = split[0].split(",");
-                String[] item = split[1].split(",");
-                if (coords.length != 4) {
-                    send(player, "[EggTimer] Expected 4x coords on line " + line);
-                    continue;
-                } else if (item.length != 4) {
-                    send(player, "[EggTimer] Expected 4x item info on line " + line);
-                    continue;
-                }
-
-                TimerEntry entry = new TimerEntry();
-                entry.world = coords[0];
-                try {
-                    entry.x = Integer.parseInt(coords[1]);
-                    entry.y = Integer.parseInt(coords[2]);
-                    entry.z = Integer.parseInt(coords[3]);
-                    entry.item = Integer.parseInt(item[0]);
-                    entry.count = Integer.parseInt(item[1]);
-                    entry.data = Integer.parseInt(item[2]);
-                    entry.interval = Integer.parseInt(item[3]);
-                }
-                catch (NumberFormatException ex) {
-                    send(player, "[EggTimer] Not a number value on line " + line);
-                    continue;
-                }
-
-                // TODO: Sanity checks.
-
-                World world = getServer().getWorld(entry.world);
-                if (world == null) {
-                    send(player, "[EggTimer] " + entry.world + " unloaded, not checking validity on line " + line);
-                } else {
-                    Block block = world.getBlockAt(entry.x, entry.y, entry.z);
-                    if (block.getType() != Material.CHEST) {
-                        send(player, "[EggTimer] Block is not a chest on line " + line);
-                        boolean found = false;
-                        for (int dx = -1; dx <= 1; ++dx) {
-                            for (int dy = -1; dy <= 1; ++dy) {
-                                for (int dz = -1; dz <= 1; ++dz) {
-                                    if (block.getRelative(dx, dy, dz).getType() == Material.CHEST) {
-                                        send(player, "(did you mean " + (entry.x+dx) + "," + (entry.y+dy) + "," + (entry.z+dz) + "?)");
-                                        found = true; break;
-                                    }
-                                }
-                                if (found) break;
-                            }
-                            if (found) break;
-                        }
-                        continue;
-                    }
-                }
-
-                timerEntries.add(entry);
-            }
-            if (player != null) send(player, "[EggTimer] Load complete, " + timerEntries.size() + " entries");
+            in = new Scanner(f);
         }
         catch (FileNotFoundException ex) {
             send(player, "[EggTimer] Could not find locations.txt.");
+            return;
         }
+        while (in.hasNextLine()) {
+            ++line;
+
+            String[] split = in.nextLine().split("=");
+            if (split.length < 2) continue;
+            if (split[0].charAt(0) == '#') continue;
+            if (split[0].charAt(0) == '$') {
+                if (split[0].equals("$requireLoaded")) {
+                    requireLoaded = Boolean.parseBoolean(split[1]);
+                } else if (split[0].equals("$clear")) {
+                    clearFirst = Boolean.parseBoolean(split[1]);
+                } else {
+                    send(player, "[EggTimer] Invalid config value on line " + line);
+                }
+                continue;
+            }
+
+            String[] coords = split[0].split(",");
+            String[] item = split[1].split(",");
+            if (coords.length != 4) {
+                send(player, "[EggTimer] Expected 4x coords on line " + line);
+                continue;
+            } else if (item.length != 4) {
+                send(player, "[EggTimer] Expected 4x item info on line " + line);
+                continue;
+            }
+
+            TimerEntry entry = new TimerEntry();
+            entry.world = coords[0];
+            try {
+                entry.x = Integer.parseInt(coords[1]);
+                entry.y = Integer.parseInt(coords[2]);
+                entry.z = Integer.parseInt(coords[3]);
+                entry.item = Integer.parseInt(item[0]);
+                entry.count = Integer.parseInt(item[1]);
+                entry.data = Integer.parseInt(item[2]);
+                entry.interval = Integer.parseInt(item[3]);
+            }
+            catch (NumberFormatException ex) {
+                send(player, "[EggTimer] Not a number value on line " + line);
+                continue;
+            }
+
+            // TODO: Sanity checks.
+
+            World world = getServer().getWorld(entry.world);
+            if (world == null) {
+                send(player, "[EggTimer] " + entry.world + " unloaded, not checking validity on line " + line);
+            } else {
+                Block block = world.getBlockAt(entry.x, entry.y, entry.z);
+                if (!(block.getState() instanceof ContainerBlock)) {
+                    send(player, "[EggTimer] Block is not a container on line " + line);
+                    boolean found = false;
+                    for (int dx = -1; dx <= 1; ++dx) {
+                        for (int dy = -1; dy <= 1; ++dy) {
+                            for (int dz = -1; dz <= 1; ++dz) {
+                                if (block.getRelative(dx, dy, dz).getType() == Material.CHEST) {
+                                    send(player, "(did you mean " + (entry.x+dx) + "," + (entry.y+dy) + "," + (entry.z+dz) + "?)");
+                                    found = true; break;
+                                }
+                            }
+                            if (found) break;
+                        }
+                        if (found) break;
+                    }
+                    continue;
+                }
+            }
+
+            timerEntries.add(entry);
+        }
+        if (player != null) send(player, "[EggTimer] Load complete, " + timerEntries.size() + " entries");
     }
 
     private void send(Player player, String text) {
